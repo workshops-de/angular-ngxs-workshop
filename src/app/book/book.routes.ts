a28a2a008a918b6cdd95435a4cc5837235fff7e5
Christian Schulz <kontakt@schulzolutions.de>
2024-02-11 16:10:04 +0100

    solve--manage-state

--- src/app/book/book.routes.ts
+++ src/app/book/book.routes.ts
@@ -4,10 +4,13 @@ import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookNewComponent } from './book-new/book-new.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookEditComponent } from './book-edit/book-edit.component';
+import { provideStore } from '@ngxs/store';
+import { BookCollectionState } from './state/book-collection.state';
 
 export const bookRoutes: Routes = [
   {
     path: '',
+    providers: [provideStore([BookCollectionState])],
     component: BookComponent,
     children: [
       {
