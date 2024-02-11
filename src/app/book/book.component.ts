26bb79749927f56e22df2b5074617235533a4a09
Christian Schulz <kontakt@schulzolutions.de>
2024-02-11 16:37:23 +0100

    solve--async-action

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookLoadAll } from './state/book-collection.actions';
 import { Store } from '@ngxs/store';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'ws-book',
@@ -10,9 +11,16 @@ import { Store } from '@ngxs/store';
   imports: [RouterOutlet]
 })
 export class BookComponent implements OnInit {
-  constructor(private readonly store: Store) {}
+  constructor(
+    private readonly store: Store,
+    private readonly snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.store.dispatch(new BookLoadAll());
+    this.store.dispatch(new BookLoadAll()).subscribe(() => {
+      this.snackBar.open('Books loaded', 'Cool', {
+        duration: 3000
+      });
+    });
   }
 }
--- src/app/book/state/book-collection.state.ts
+++ src/app/book/state/book-collection.state.ts
@@ -2,49 +2,8 @@ import { Action, createSelector, Selector, State, StateContext } from '@ngxs/sto
 import { Injectable } from '@angular/core';
 import { BookLoadAll } from './book-collection.actions';
 import { BookCollectionStateModel } from './book-collection.model';
-import { Book } from '../models';
-
-const books: Book[] = [
-  {
-    id: '1001606140805',
-    title: 'Java Web Scraping Handbook',
-    subtitle: 'Learn advanced Web Scraping techniques',
-    isbn: '1001606140805',
-    abstract:
-      'Web scraping or crawling is the art of fetching data from a third party website by downloading and parsing the HTML code to extract the data you want. It can be hard. From bad HTML code to heavy Javascript use and anti-bot techniques, it is often tricky. Lots of companies use it to obtain knowledge ...',
-    author: 'Kevin Sahin',
-    publisher: 'Leanpub',
-    price: 10,
-    numPages: 115,
-    cover: 'http://localhost:4730/covers/1001606140805.png'
-  },
-  {
-    id: '9780071494618',
-    title: 'Hacking Exposed Web 2.0',
-    subtitle: 'Web 2.0 Security Secrets and Solutions',
-    isbn: '9780071494618',
-    abstract:
-      'Protect your Web 2.0 architecture against the latest wave of cybercrime using expert tactics from Internet security professionals. Hacking Exposed Web 2.0 shows how hackers perform reconnaissance, choose their entry point, and attack Web 2.0 - based services, and reveals detailed countermeasures and...',
-    author: 'Rich Cannings, Himanshu Dwivedi, Zane Lackey',
-    publisher: 'McGraw-Hill',
-    price: 10,
-    numPages: 258,
-    cover: 'http://localhost:4730/covers/9780071494618.png'
-  },
-  {
-    id: '9780071740647',
-    title: 'Hacking Exposed Web Applications, 3rd Edition',
-    subtitle: 'Web Applications Security Secrets and Solutions',
-    isbn: '9780071740647',
-    abstract:
-      "Protect your Web applications from malicious attacks by mastering the weapons and thought processes of today's hacker. Written by recognized security practitioners and thought leaders, Hacking Exposed Web Applications, Third Edition is fully updated to cover new infiltration methods and countermeasu...",
-    author: 'Joel Scambray, Vincent Liu, Caleb Sima',
-    publisher: 'McGraw-Hill',
-    price: 10,
-    numPages: 482,
-    cover: 'http://localhost:4730/covers/9780071740647.png'
-  }
-];
+import { BookApiService } from '../book-api.service';
+import { tap } from 'rxjs/operators';
 
 @State<BookCollectionStateModel>({
   name: 'bookCollection',
@@ -67,10 +26,15 @@ export class BookCollectionState {
 
   @Action(BookLoadAll)
   loadAll(ctx: StateContext<BookCollectionStateModel>) {
-    const state = ctx.getState();
-    ctx.setState({
-      ...state,
-      entities: [...books]
-    });
+    return this.bookService.getAll().pipe(
+      tap(books =>
+        ctx.setState(state => ({
+          ...state,
+          entities: books
+        }))
+      )
+    );
   }
+
+  constructor(private readonly bookService: BookApiService) {}
 }
