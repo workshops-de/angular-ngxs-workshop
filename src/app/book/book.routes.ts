f04ffb17d14498224f39e2c840446bd38f571fa6
Dave <dave@webdave.de>
2024-03-25 08:49:26 +0100

    Manage State

--- src/app/book/book.routes.ts
+++ src/app/book/book.routes.ts
@@ -4,11 +4,14 @@ import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookNewComponent } from './book-new/book-new.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookEditComponent } from './book-edit/book-edit.component';
+import { BookCollectionState } from './state/book-collection.state';
+import { provideStore } from '@ngxs/store';
 
 export const bookRoutes: Routes = [
   {
     path: '',
     component: BookComponent,
+    providers: [provideStore([BookCollectionState])],
     children: [
       {
         path: '',
---
+++ src/app/book/state/book-collection.model.ts
@@ -0,0 +1,5 @@
+import { Book } from '../models';
+
+export interface BookCollectionStateModel {
+  entities: Book[];
+}
